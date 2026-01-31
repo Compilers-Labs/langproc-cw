6bd4061359e5cd8f596bb1fde66cf9a666ecbbd5
Your Name <you@example.com>
2026-01-31 01:49:58 +0000

    type specifier comments

--- include/ast_type_specifier.hpp
+++ include/ast_type_specifier.hpp
@@ -1,5 +1,27 @@
 #pragma once
 
+/*
+
+We have an enum here TypeSpecifier which for now only has INT
+
+Then we have a templated operator function of type LogStream for the operator <<
+this takes a LogStream by ref and a TypeSpecifier
+
+Inside is a lambda function which takes in this type specifier
+if the type specifier is of type int then it returns the string "int", otherwise an error is thrown
+
+then the function returns logstream << the lamda function.
+
+so e.g if i did:
+
+TypeSpecifier t = TypeSpecifier INT
+std::cout << t
+
+this would output int to the console.
+
+*/
+
+
 #include <stdexcept>
 #include <string_view>
 
