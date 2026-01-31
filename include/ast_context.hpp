481844ceec3371380fdab631ebf716350e4c2d21
Your Name <you@example.com>
2026-01-31 18:13:16 +0000

    declare initial variables and get register implementation coauthored by Yichan

--- include/ast_context.hpp
+++ include/ast_context.hpp
@@ -1,23 +1,38 @@
 #pragma once
+#include <iostream>
+#include <memory>
+#include <stdexcept>
+#include <string>
 #include <unordered_map>
+#include <vector>
 
 namespace ast {
+
+class Instruction;
 // An object of class Context is passed between ast nodes during compilation.
 // This can be used to pass around information about what's currently being
 // compiled (e.g. function scope and variable names).
+
+
 class Context
 {
 private:
     unsigned pc;//program counter
-    //map of register names to content of registers (only int in this case)
-    // shared pointers to a vector of instructions
-    //map of labels to instruction lines
+    //CHANGE TO TEMPLATE VARIABLE
+    std::unordered_map<std::string,int32_t> register_map; //map of register names to content of registers
+    std::vector<std::shared_ptr<Instruction>> instruction_list; // shared pointers to a vector of instructions
+    std::unordered_map<std::string, unsigned> label_map; //map of labels to instruction lines
 public:
-    Context()
-        : pc(0)
-    {}
+    Context() : pc(0){}
 
-    int32_t getRegister(std::string name);
+    int32_t getRegister(std::string name){
+
+        std::cerr << "getRegister(" << name << ")\n";
+        if (!register_map.count(name)){
+            throw std::runtime_error("getRegister("+name+") : Register does not exist");
+        }
+        return register_map[name];
+    }
 
 
     void setRegister(std::string name, int32_t value);
