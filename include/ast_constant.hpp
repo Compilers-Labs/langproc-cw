1d5ec92e06c9f3ff2f679f9eaa75ea1c96199fee
Your Name <you@example.com>
2026-01-31 01:17:54 +0000

    notes on ast_constant

--- include/ast_constant.hpp
+++ include/ast_constant.hpp
@@ -1,5 +1,16 @@
 #pragma once
 
+/*
+
+This is an ast node in namespace ast, each node has a private value and the constructor takes in that value
+It has 2 functions:
+1. EmitRISC which takes in an output stream to output to and context of type Context -> ?
+2. Print which takes in a stream
+
+they are both void return types and marked as const override so they are written somewhere else.
+
+*/
+
 #include "ast_node.hpp"
 
 namespace ast {
