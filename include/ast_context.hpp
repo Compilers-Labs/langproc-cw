3b9ef0ddea5eed28339ec1d10779f5c31f210b24
Your Name <you@example.com>
2026-01-31 18:02:50 +0000

    setup context file coauthered Yichan

--- include/ast_context.hpp
+++ include/ast_context.hpp
@@ -1,11 +1,34 @@
 #pragma once
+#include <unordered_map>
 
 namespace ast {
 // An object of class Context is passed between ast nodes during compilation.
 // This can be used to pass around information about what's currently being
 // compiled (e.g. function scope and variable names).
-class Context {
-  /* TODO decide what goes inside here */
-};
+class Context
+{
+private:
+    unsigned pc;//program counter
+    //map of register names to content of registers (only int in this case)
+    // shared pointers to a vector of instructions
+    //map of labels to instruction lines
+public:
+    Context()
+        : pc(0)
+    {}
 
-}  // namespace ast
+    int32_t getRegister(std::string name);
+
+
+    void setRegister(std::string name, int32_t value);
+
+    void PCPlusOne();
+
+    void jump(std::string label);
+
+    void step();
+
+    void add_instruction(const std::string &line);
+};
+} // namespace ast
+
