53e86f020739b6bb02e11a25c8334b3a8be015eb
Your Name <you@example.com>
2026-01-31 02:14:50 +0000

    add comments to cli

--- include/cli.hpp
+++ include/cli.hpp
@@ -1,5 +1,16 @@
 #pragma once
 
+/*
+
+This just defines a struct CommandLineArguments, Takes in source path and out path.
+I imagine we will use this struct to store args when calling the main function
+in this is the Source path and the Out path
+
+very similar to gcc in.cpp -o out.asm
+
+*/
+
+
 #include <unistd.h>
 
 #include <iostream>
